f04543f2efc957de47f91a958d4b75399af56aa9
John Johnson <jrjohnson1231@gmail.com>
2018-01-09 14:41:13 -0500

    got basic prices for some currencies

---
+++ main.cr
@@ -0,0 +1,3 @@
+require "./src/*"
+
+Cryptoquartz.run
--- src/cryptoquartz.cr
+++ src/cryptoquartz.cr
@@ -1,6 +1,20 @@
 require "./cryptoquartz/*"
+require "cryptocompare"
 
 # TODO: Write documentation for `Cryptoquartz`
 module Cryptoquartz
-  # TODO: Put your code here
+  extend self
+
+  def get_rate(curr1, curr2, opts = {} of Char => String)
+    Cryptocompare::Price.find(curr1, curr2, opts)[curr1][curr2].as_f
+  end
+
+  def run
+    btc_price = self.get_rate("BTC", "USD", {'e' => "Coinbase"})
+    trx_price = self.get_rate("TRX", "BTC", {'e' => "Binance"}) * btc_price
+    xvg_price = self.get_rate("XVG", "BTC", {'e' => "Binance"}) * btc_price
+    puts "BTC: #{btc_price}"
+    puts "TRX: #{trx_price}"
+    puts "XVG: #{xvg_price}"
+  end
 end
